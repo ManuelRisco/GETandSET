e4c4f8a5faa241cebfdc9a72368a86136f6be224
Manuel <manuelfabrizzio123@gmail.com>
2024-04-18 12:21:59 -0500

    prueba

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ class persona {
         this._apellido = apellido;
         this._localidad = localidad;
     }
+
     // Metodo GET
-    get nombre () {
+    get nombre () { // cuando esta vacio los parametros, es porque el valor solo se devolvera de un valor que ya se configuro
         return this._nombre;
     }
-    get apellido () {
+    get apellido () { 
         return this._apellido;
     }
-    get localidad () {
+    get localidad () { 
         return this.localidad;
     }
     
     // Metodo SET
-    set nombre (nombre){
+    set nombre (nombre){ // como modifica un valor, se le debe poner un parametro
         this._nombre = nombre;
     }
     set apellido (apellido) {
@@ -26,25 +27,26 @@ class persona {
         this._localidad = localidad;
     }
 
+    // Metodo GET - total
 
     get total () {
         return `${this._nombre}, ${this._apellido}, ${this._localidad}`;
     }
 
 }
-let cliente = new persona ('manuel', 'risco', 'chorrillos');
+let cliente = new persona ("manuel", "risco", "chorrillos");
 
-console.log(cliente); //primero
+console.log(cliente.total); //primero
 
 cliente.nombre = 'roberto'; //cambia el nombre
-console.log(cliente);
+console.log(cliente.total);
 
 cliente.apellido = 'tapia'; // se le cambia del anterior el apellido
-console.log(cliente);
+console.log(cliente.total);
 
 cliente.localidad = 'lima'; // se le cambia del anterior la localidad
-console.log(cliente);
+console.log(cliente.total);
 
-console.log(cliente.total); // se obtiene todos los datos cambiados
+console.log(cliente.total); // se obtiene todos los datos cambiados................................. me sale persona {_nombre: 'manuel', _apellido: 'risco', _localidad: 'chorrillos'}
 
 
